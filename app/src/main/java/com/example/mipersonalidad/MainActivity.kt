2c0b9e606b975a218665447d09d9372bf78f6691
Arturo Curiel <curieldaz@gmail.com>
2024-08-22 18:50:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mipersonalidad/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mipersonalidad
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextClock
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, TestSelectionFragment())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/example/mipersonalidad/models/Question.kt
@@ -0,0 +1,7 @@
+package com.example.mipersonalidad.models
+
+class Question (
+    val id: Int,
+    val text: String,
+    val options: List<String>,  // List of options for the answers
+)
